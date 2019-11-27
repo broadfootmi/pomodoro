e3b1dd8c5f02b3ceeef60c257e26e47ef71b2801
Mikael Broadfoot <broadfootmi@gmail.com>
2019-11-27 18:07:55 -0500

    format time string to minutes:seconds

--- app/src/main/java/org/androidcourse/pomodorotimer/MainActivity.java
+++ app/src/main/java/org/androidcourse/pomodorotimer/MainActivity.java
@@ -2,13 +2,49 @@ package org.androidcourse.pomodorotimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView timerTextView = findViewById(R.id.timerTextView);
+
+
+        timer = new CountDownTimer(3000, 500){
+
+            @Override
+            public void onTick(long millisecondsRemaining) {
+                long minutes = TimeUnit.MILLISECONDS.toMinutes(millisecondsRemaining);
+                long seconds = TimeUnit.MILLISECONDS.toSeconds(millisecondsRemaining) -
+                        TimeUnit.MINUTES.toSeconds(minutes);
+
+                timerTextView.setText(
+                        getString(
+                                R.string.time,
+                                minutes,
+                                seconds
+                                )
+                );
+            }
+
+            @Override
+            public void onFinish() {
+            }
+        }.start();
+
+
     }
 }
